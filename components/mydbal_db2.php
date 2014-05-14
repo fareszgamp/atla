5f97baeadd2281cd804ca980b627f13fd9b61378
fareszgamp <faresz@pannonmail.hu>
2014-05-14 19:20:54 +0200

    Új mvc

---
+++ components/mydbal_db2.php
@@ -0,0 +1,16 @@
+<?php
+   /*
+   //Oracle
+   define( "SERVERNAME", "" );
+   define( "USERNAME", "felhnev" );
+   define( "PWD", "jelszo" );
+   define( "DBNAME", "adatbazis" );
+   require "dbal_oracle.php";
+   */
+   
+   //MySQL
+   define( "DB_SERVER", "localhost" );
+   define( "DB_USER", "root" );
+   define( "DB_PASS", "123456" );
+   define( "DB_NAME", "atlantisz" );
+   require "../components/dbbase.php";
